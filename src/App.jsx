cda9ac6e2c87dd666203048c3f50170e2814a69a
swon95 <shimtkddnjs13@gmail.com>
2023-10-22 15:50:36 +0900

    feat: Todo Item & List

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,69 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
+let todoItemId = 0;
+
+const TodoItemInputField = (props) => {
+    const [input, setInput] = useState("");
+    // console.log(input);
+
+    const onSubmit = () => {
+        // TodoItemInputField 컴포넌트에서 사용하는 input 을 props
+        props.onSubmit(input);
+        setInput("");
+    };
+    return (
+        <>
+            <TextField
+                id="todo-item-input"
+                label="Todo Item"
+                variant="outlined"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <Button variant="outlined" onClick={onSubmit}>
+                Submit
+            </Button>
+        </>
+    );
+};
+
+const TodoItem = (props) => {
+    return (
+        <li>
+            <span>{props.todoItem.todoItemContent}</span>
+        </li>
+    );
+};
+
+const TodoItemList = (props) => {
+    const todoList = props.todoItemList.map((todoItem, index) => {
+        return <TodoItem key={todoItem.id} todoItem={todoItem} />;
+    });
+    return (
+        <>
+            <ul>{todoList}</ul>
+        </>
+    );
+};
+
 function App() {
-    const TodoItemInputField = (props) => {
-        const [input, setInput] = useState("");
-        // console.log(input);
+    const [todoItemList, setTodoItemList] = useState([]);
 
-        const onSubmit = () => {
-            // TodoItemInputField 컴포넌트에서 사용하는 input 을 props
-            props.onSubmit(input);
-            setInput("");
-        };
-        return (
-            <>
-                <TextField
-                    id="todo-item-input"
-                    label="Todo Item"
-                    variant="outlined"
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                />
-                <Button variant="outlined" onClick={onSubmit}>
-                    Submit
-                </Button>
-            </>
-        );
+    const newSubmit = (newTodoItem) => {
+        setTodoItemList([
+            ...todoItemList,
+            {
+                id: todoItemId++,
+                todoItemContent: newTodoItem,
+                isFinished: false,
+            },
+        ]);
     };
-
-    const TodoItemList = (props) => {
-        const todoList = props.todoItemList.map((todoItem, index) => {
-            return <li key={index}>{todoItem.todoItemContent}</li>;
-        });
-        return (
-            <>
-                <ul>{todoList}</ul>
-            </>
-        );
-    };
-
     return (
         <div className="App">
-            <TodoItemInputField
-                onSubmit={(input) => {
-                    console.log(input);
-                }}
-            />
-            <TodoItemList
-                todoItemList={[
-                    {
-                        todoItemContent: "오늘 할일",
-                    },
-                ]}
-            />
+            <TodoItemInputField onSubmit={newSubmit} />
+            <TodoItemList todoItemList={todoItemList} />
         </div>
     );
 }
